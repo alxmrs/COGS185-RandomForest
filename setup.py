4c0725f182a35053e80b3d51c24c9bd642325e33
Alex Rosengarten <alexrosengarten@gmail.com>
2016-04-22 19:49:22 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='random_forest',
+      version='0.1',
+      description='My implementation of the Random Forest algorithm, using ID3 decision trees',
+      url='https://github.com/alxrsngrtn/COGS185-RandomForest',
+      author='Alex Rosengarten',
+      author_email='alexrosengarten@gmail.com',
+      license='MIT',
+      packages=['random_forest'],
+      zip_safe=False)
